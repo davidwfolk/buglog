17cb2e158f0b699461f2386d680d735f90f0f8eb
David Folk <davidwfolk@gmail.com>
2020-04-26 13:52:13 -0600

    working on styling

--- clientsrc/src/Pages/Bug.vue
+++ clientsrc/src/Pages/Bug.vue
@@ -2,12 +2,41 @@
   <div class="board container-fluid bg-dimg">
     <div class="row ml-2">
       <div class="mt-5 card col-12">
+          <div v-if="!edit">
         <h5 class="text-primary mt-2 p-0 m-0 border-primary">
           <span class="text-info">Bug Issue:</span>
         </h5>
         <h2 class="text-primary">
           <strong>{{bug.title}}</strong>
+        <button class="btn btn-sm textColor" @click="edit = true">
+              <svg
+                class="bi bi-pencil"
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"
+                  clip-rule="evenodd"
+                />
+                <path
+                  fill-rule="evenodd"
+                  d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
         </h2>
+          </div>
+          <div v-else>
+            <form @submit.prevent="editBug()">
+              <input type="text" v-model="bug.title" />
+              <button class="btn text-warning">submit</button>
+            </form>
+          </div>
         <h5 class="text-primary mt-2 p-0 m-0 border-primary">
           <span class="text-info">Reported By:</span>
         </h5>
@@ -19,28 +48,60 @@
           </h4>
         </h2>
         <div class="card">
-          <h5>{{bug.description}}</h5>
-        </div>
-        <div class="col-1 mt-2 ml-auto">
-        <button class="btn btn-danger" @click="closeBug()">Close</button>
-        </div>
-      <div class="row p-2">
-        <div class="col-12">
-          <div id="card-display" class="card border-0 bg-transparent text-light">
-            <note v-for="note in notes" :noteData="note" :key="note._id"></note>
-            <form @submit.prevent="addNewNote()">
-              <input
-                type="text"
-                v-model="newNote.title"
-                class="bg-light text-dark input-group-sm border-0 ml-2 rounded-lg mt-2"
-                placeholder=" New Note"
-              />
-              <button class="display-5 btn m-0 p-0" type="submit">
-                <strong>+</strong>
-              </button>
+          <div v-if="!editing">
+            <h5 class="px-2">{{bug.description}}</h5>
+            <button class="btn btn-sm textColor" @click="editing = true">
+              <svg
+                class="bi bi-pencil"
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"
+                  clip-rule="evenodd"
+                />
+                <path
+                  fill-rule="evenodd"
+                  d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+          <div v-else>
+            <form @submit.prevent="editBug()">
+              <input type="text" v-model="bug.description" />
+              <button class="btn text-warning">submit</button>
             </form>
           </div>
-      </div>
+          <div class="col-1 mt-2 ml-auto">
+            <button class="btn btn-danger" @click="closeBug()">Close</button>
+          </div>
+          <div class="row p-2">
+            <div class="col-12">
+              <div id="card-display" class="card border-0 bg-transparent text-light">
+                <note v-for="note in notes" :noteData="note" :key="note._id"></note>
+                <div v-if="!closed">
+                  <form @submit.prevent="addNewNote()">
+                    <input
+                      type="text"
+                      v-model="newNote.title"
+                      class="bg-light text-dark input-group-sm border-0 ml-2 rounded-lg mt-2"
+                      placeholder=" New Note"
+                    />
+                    <button class="display-5 btn m-0 p-0" type="submit">
+                      <strong>+</strong>
+                    </button>
+                  </form>
+                </div>
+                <div v-else></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -57,7 +118,9 @@ export default {
   data() {
     return {
       newNote: {},
-      false: "Open"
+      editing:false ,
+      edit: false,
+      closed: false
     };
   },
   mounted() {
@@ -85,11 +148,18 @@ export default {
       this.newNote = {};
     },
     closeBug() {
-      if(confirm('are you sure you want to close this ticket?')) {
-      this.bug.closed = true
-      this.$store.dispatch("editBug", this.bug)
-    }
-    }
+      if (confirm("are you sure you want to close this ticket?")) {
+        this.closed = true;
+        this.$store.dispatch("editBug", this.bug);
+
+
+      }
+    },
+    editBug() {
+      this.$store.dispatch("editBug", this.bug);
+      this.editing = false;
+      this.edit = false
+    },
   },
   components: {
     Note
--- clientsrc/src/components/Note.vue
+++ clientsrc/src/components/Note.vue
@@ -1,12 +1,14 @@
 <template>
-<div class="note card bg-success col-12 mr-1 mb-2 p-2 border-0 rounded-lg">
+  <div class="note card bg-success col-12 mr-1 mb-2 p-2 border-0 rounded-lg">
     <div v-if="!editing">
       <div class="row">
         <div class="col-12">
           <div class="row">
-            <h5 class="col-6 p-0 ml-4  mt-1 mb-1"><strong>{{noteData.title}}</strong></h5>
-            <div class="col-1 ml-auto text-right">
-              <button class="btn btn-sm textColor" @click="editing = true">
+            <h5 class="col-6 p-0 mr-auto ml-4 mt-1 mb-1">
+              <strong>{{noteData.title}}</strong>
+            </h5>
+            <div class="col-1 ml-auto mr-4 text-right">
+              <!-- <button class="btn btn-sm textColor" @click="editing = true">
                 <svg
                   class="bi bi-pencil"
                   width="1em"
@@ -26,69 +28,70 @@
                     clip-rule="evenodd"
                   />
                 </svg>
-              </button>
-              <button class="btn btn-sm text-danger" @click="deleteNote()">
-                <svg
-                  class="bi bi-trash"
-                  width="1em"
-                  height="1em"
-                  viewBox="0 0 16 16"
-                  fill="currentColor"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"
-                  />
-                  <path
-                    fill-rule="evenodd"
-                    d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </button>
-            </div>
+            </button>-->
+            <button class="btn btn-sm text-danger" @click="deleteNote()">
+              <svg
+                class="bi bi-trash"
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"
+                />
+                <path
+                  fill-rule="evenodd"
+                  d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
     </div>
-    <div v-else>
+  </div>
+  <!-- <div v-else>
       <form @submit.prevent="editNote()">
         <input type="text" v-model="noteData.title" />
         <button class="btn text-warning">submit</button>
       </form>
-    </div>
+  </div>-->
   </div>
 </template>
 
 
 <script>
 export default {
-  name: 'note',
-  props:["noteData"],
-  data(){
+  name: "note",
+  props: ["noteData"],
+  data() {
     return {
       editing: false
-    }
+    };
   },
-  computed:{
+  computed: {
     profile() {
       return this.$store.state.user;
-    },
+    }
   },
-  methods:{
-      deleteNote() {
+  methods: {
+    deleteNote() {
+    if (confirm("are you sure you want to close this ticket?")) {
       this.$store.dispatch("deleteNote", this.noteData);
+    }
     },
     editNote() {
       this.$store.dispatch("editNote", this.noteData);
       this.editing = false;
     }
   },
-  components:{}
-}
+  components: {}
+};
 </script>
 
 
 <style scoped>
-
 </style>
